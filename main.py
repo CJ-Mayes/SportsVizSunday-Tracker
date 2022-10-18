9493e1a6587d0d29af29b14f87156e43835aaf54
Mayes <Cj.Mayes@eu.jll.com>
2022-10-18 19:29:09 +0100

    commit

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ except BaseException as e:
     print('Status Failed On,', str(e))
 
 tweets_df.to_csv('SportsVizSunday_Latest.csv', encoding='utf-8', index=False)
-tweets_df.to_csv('SportsVizSunday_Archive.csv', encoding='utf-8', index=False)
+pd.read_csv('SportsVizSunday_Archive.csv').append(tweets_df).drop_duplicates().to_csv('SportsVizSunday_Archive.csv')
